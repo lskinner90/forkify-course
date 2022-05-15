dbc915a805616a42657d0d85f3cb0c9e9d7dd7d3
lskinner90 <lukeandrewskinner@gmail.com>
2022-05-15 11:44:42 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '76c4d400-4469-45d8-86bb-89899f55e003';
+export const MODAL_CLOSE_SEC = 2.5;
